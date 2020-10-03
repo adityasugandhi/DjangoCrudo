b6a47623f0c771035ebf4fd12aaa2cce6d9ab221
adityasugandhi <adityasugandhi@hotmai.com>
2020-10-03 14:03:00 +0530

    Connect MongoDb with the backend

--- Finalver/Finalver/settings.py
+++ Finalver/Finalver/settings.py
@@ -77,9 +77,11 @@ WSGI_APPLICATION = 'Finalver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'djongo',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'HOST' : 'cluster0.axvys.mongodb.net',
+        'NAME': 'green',
+         }
 }
 
 
